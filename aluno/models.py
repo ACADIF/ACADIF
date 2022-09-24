bf1f400bd4438f9c9a8aebd1951e57e423fe4808
Rondineleflorencio <70039605+Rondineleflorencio@users.noreply.github.com>
2022-09-23 23:23:21 -0300

    Revert "aprimoramento do professor"

--- aluno/models.py
+++ aluno/models.py
@@ -3,13 +3,5 @@ from django.db import models
 
 class Aluno(models.Model):
     """Dados do ALUNO"""
-    alvos = (
-        ("Ci","Concluiu"),
-        ("Co","Cursando")
-    )
     nome = models.CharField(max_length=30)
-    matricula = models.IntegerField(primary_key=True)
-    cpf = models.IntegerField(unique=True)
-    born = models.DateField()
-    endereco = models.CharField(max_length=120)
-    estado = models.CharField(max_length=210, choices=alvos)
+    mat = models.IntegerField(primary_key=True)
